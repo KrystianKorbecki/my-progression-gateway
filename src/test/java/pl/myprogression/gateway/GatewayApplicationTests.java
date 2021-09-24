d8f9d8ed7f70ad0f069bb3082a57685329494370
KrystianKorbecki <krystian.korbecki36@interia.pl>
2021-09-24 18:38:08 +0200

    init commit

---
+++ src/test/java/pl/myprogression/gateway/GatewayApplicationTests.java
@@ -0,0 +1,15 @@
+package pl.myprogression.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
